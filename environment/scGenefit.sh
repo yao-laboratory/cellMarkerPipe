6c189cc803128a68dfccc54078bdda6094f83c43
PengchongMa <mpengchong@gmail.com>
2024-05-01 14:20:09 -0500

    Update scGenefit.sh
    fix import scGenefit function errors

--- environment/scGenefit.sh
+++ environment/scGenefit.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-cp environment/scGenefit.yaml ./
+# cp environment/scGenefit.yaml ./
+# conda env create -f scGenefit.yaml
+# conda activate scGenefit_env
+# pip install -e .
+
+cp scGenefit.yaml ../
+cd ..
+export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
 conda env create -f scGenefit.yaml
-conda activate scGenefit_env
+source activate scGenefit_env
+pip install scGeneFit
 pip install -e .
