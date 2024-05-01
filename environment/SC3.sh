d79282fd7d74f564c419a634ff0a7608e629ebfe
PengchongMa <mpengchong@gmail.com>
2024-05-01 16:04:20 -0500

    Update SC3.sh
    fix sc3 issues

--- environment/SC3.sh
+++ environment/SC3.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-cp environment/SC3.yaml ./
+# cp environment/SC3.yaml ./
+# conda env create -f SC3.yaml
+# conda activate sc3_env
+# pip install -e .
+
+cp SC3.yaml ../
+cd ..
 conda env create -f SC3.yaml
-conda activate sc3_env
+source activate sc3_env
+
+# Install R packages
+R --vanilla << EOF
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("SC3")
+EOF
 pip install -e .
