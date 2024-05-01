41416af17079c0ffdc1c232a40572e56451017d2
PengchongMa <mpengchong@gmail.com>
2024-05-01 13:04:53 -0500

    Update seurat.sh
    fix errors cp: cannot stat'environment/seurat.yaml'

--- environment/seurat.sh
+++ environment/seurat.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cp environment/seurat.yaml ./
+# cp environment/seurat.yaml ./
+# conda env create -f seurat.yaml
+# conda activate seurat_env
+# pip install -e .
+
+cp seurat.yaml ../
+cd ..
 conda env create -f seurat.yaml
-conda activate seurat_env
+source activate seurat_env
 pip install -e .
