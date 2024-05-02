e22d60a01f3a5cefa044e43aa7e1d337b8e6bc57
PengchongMa <mpengchong@gmail.com>
2024-05-02 13:15:06 -0500

    Update highvariable.sh

--- environment/highvariable.sh
+++ environment/highvariable.sh
@@ -2,5 +2,7 @@
 
 cp environment/highvariable.yaml ./
 conda env create -f highvariable.yaml
-conda activate highvariable_env
+source activate highvariable_env
+# Run the R commands to install the SCMarker package
+R -e "devtools::install_github('KChen-lab/SCMarker')"
 pip install -e .
