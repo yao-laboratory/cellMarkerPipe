ee2fbab93500ba6adf2b23e9fcfb678bf844252e
PengchongMa <mpengchong@gmail.com>
2024-05-02 13:05:20 -0500

    Update comet.sh

--- environment/comet.sh
+++ environment/comet.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# cp environment/comet.yaml ./
-# conda env create -f comet.yaml
-# conda activate COMET_env
-# pip install -e .
-
-cp comet.yaml ../
-cd ..
+cp environment/comet.yaml ./
 conda env create -f comet.yaml
-source activate COMET_env
+conda activate COMET_env
 pip install -e .
