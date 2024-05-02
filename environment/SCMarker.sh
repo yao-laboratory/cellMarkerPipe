433995fa593ad4e0d3e4d319f16ad89a9121b593
PengchongMa <mpengchong@gmail.com>
2024-05-02 13:05:59 -0500

    Update SCMarker.sh

--- environment/SCMarker.sh
+++ environment/SCMarker.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# cp environment/SCMarker.yaml ./
-# conda env create -f SCMarker.yaml
-# conda activate SCMarker_env
-# pip install -e .
-
-cp SCMarker.yaml ../
-cd ..
+cp environment/SCMarker.yaml ./
 conda env create -f SCMarker.yaml
 source activate SCMarker_env
-
 # Run the R commands to install the SCMarker package
 R -e "devtools::install_github('KChen-lab/SCMarker')"
-
 pip install -e .
