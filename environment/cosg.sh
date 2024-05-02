fa616659b0643b4819a7231b191658f58f954eaf
PengchongMa <mpengchong@gmail.com>
2024-05-02 13:04:58 -0500

    Update cosg.sh

--- environment/cosg.sh
+++ environment/cosg.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# cp environment/cosg.yaml ./
-# conda env create -f cosg.yaml
-# conda activate COSGR_env
-# pip install -e .
-
-cp cosg.yaml ../
-cd ..
+cp environment/cosg.yaml ./
 conda env create -f cosg.yaml
 source activate COSGR_env
-
 # Install R packages
 Rscript -e "if (!requireNamespace('remotes', quietly = TRUE)) install.packages('remotes')"
 Rscript -e "remotes::install_github('genecell/COSGR')"
