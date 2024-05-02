69ffd82929b745aebb3558a9ccf188a2e7714971
PengchongMa <mpengchong@gmail.com>
2024-05-02 13:03:24 -0500

    Update scGenefit.sh

--- environment/scGenefit.sh
+++ environment/scGenefit.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-# cp environment/scGenefit.yaml ./
-# conda env create -f scGenefit.yaml
-# conda activate scGenefit_env
-# pip install -e .
-
-cp scGenefit.yaml ../
-cd ..
+cp environment/scGenefit.yaml ./
 export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
 conda env create -f scGenefit.yaml
 source activate scGenefit_env
