a231f210562f132feb15039dcd61046a517f333a
PengchongMa <mpengchong@gmail.com>
2024-05-02 13:04:19 -0500

    Update SC3.sh

--- environment/SC3.sh
+++ environment/SC3.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# cp environment/SC3.yaml ./
-# conda env create -f SC3.yaml
-# conda activate sc3_env
-# pip install -e .
-
-cp SC3.yaml ../
-cd ..
+cp environment/SC3.yaml ./
 conda env create -f SC3.yaml
 source activate sc3_env
-
 # Install R packages
 R --vanilla << EOF
 if (!require("BiocManager", quietly = TRUE))
